7690293d3123857e0d00cb2f3f021eb2a34eb973
Phan Dang <phannhatdang@phans-macbook-air.local>
2018-09-10 06:09:26 +0700

    Add GitHub test

---
+++ CurrencyTextfield/Github test file.swift
@@ -0,0 +1,9 @@
+//
+//  Github test file.swift
+//  CurrencyTextfield
+//
+//  Created by Phan Nhat Dang on 9/10/18.
+//  Copyright © 2018 Phan Nhat Dang. All rights reserved.
+//
+
+import Foundation
